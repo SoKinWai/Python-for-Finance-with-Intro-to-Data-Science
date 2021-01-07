18429268cf96c8b3c03cdba8302e93edc4ebeaa9
SoKinWai <45207705+SoKinWai@users.noreply.github.com>
2021-01-07 16:59:20 -0600

    Add files via upload

---
+++ Level 1/Section 1.1/Exercise 4/1.1.4.py
@@ -0,0 +1,12 @@
+#1.1.4
+#Create a program that takes input from the user (using the input function),
+# and stores it in a variable.
+
+def main():
+    #Code goes here
+
+    integer=input('Please enter an integer:')
+    print(integer)
+
+if __name__ == '__main__':
+    main()
