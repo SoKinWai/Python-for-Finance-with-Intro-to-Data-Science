7bc0d0eba46416b33a43b0578646372e3120ef16
SoKinWai <45207705+SoKinWai@users.noreply.github.com>
2021-01-07 16:58:03 -0600

    Add files via upload

---
+++ Level 1/Section 1.1/Exercise 3/1.1.3.py
@@ -0,0 +1,9 @@
+#1.1.3
+#Do the same as exercise 1), but the caveat here is that each word should be separated with a tab.
+
+def main():
+    #Code goes here
+    print('Hello\tWorld!')
+
+if __name__ == '__main__':
+    main()
