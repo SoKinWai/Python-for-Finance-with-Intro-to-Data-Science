952b1881401895b7ae63aa5bf5fe667fd1177c48
SoKinWai <45207705+SoKinWai@users.noreply.github.com>
2021-01-07 16:54:30 -0600

    Add files via upload

---
+++ Level 1/Section 1.1/Exercise 2/1.1.2.py
@@ -0,0 +1,9 @@
+#1.1.2
+#Do the same as exercise 1), but the caveat here is that each word should appear on a different line.
+
+def main():
+    #Code goes here
+    print('Hello\nWorld!')
+
+if __name__ == '__main__':
+    main()
