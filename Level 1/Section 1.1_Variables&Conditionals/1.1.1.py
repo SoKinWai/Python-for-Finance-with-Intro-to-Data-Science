8c7706de223aaaba7980f755252a9944eedb6d94
SoKinWai <45207705+SoKinWai@users.noreply.github.com>
2022-04-06 13:48:12 -0500

    Add files via upload

---
+++ Level 1/Section 1.1_Variables&Conditionals/1.1.1.py
@@ -0,0 +1,16 @@
+#Student name: Jianwei Su
+#Date: 04/05/2022
+#1.1.1
+
+
+def main():
+    # Code goes here
+    print('\n==========Exercise 1.1.1==========')
+    print('***A program that outputs ‘Hello World!’ on screen***\n')
+
+    #It prints 'Hello World!' on screen.
+    print('Hello World!')
+    print('\n***Program Complete***')
+
+if __name__ == '__main__':
+    main()
