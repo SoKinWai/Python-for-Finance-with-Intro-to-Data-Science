d02158db39a3672abb53314ac52d6337b82b9f64
SoKinWai <45207705+SoKinWai@users.noreply.github.com>
2022-04-10 14:50:02 -0500

    Add files via upload

---
+++ Level 1/Section 1.6_Packages/1.6.2/Sub_a/Hello_world.py
@@ -0,0 +1,7 @@
+#c. Create one or more Python scripts in each subfolder, at each level.
+# Each script should have a unique function (you may reuse previously-created functions and/or create new functions).
+# Be sure to name the scripts aptly.
+
+
+def Hello_world():
+    return print('Hello World!')
