6da2dee9bd802bcb4e5f3a688226eba65cbd278e
SoKinWai <45207705+SoKinWai@users.noreply.github.com>
2022-04-10 14:56:00 -0500

    Add files via upload

---
+++ Level 1/Section 1.6_Packages/1.6.2/Sub_b/Mortgage_amounts.py
@@ -0,0 +1,7 @@
+#c. Create one or more Python scripts in each subfolder, at each level.
+# Each script should have a unique function (you may reuse previously-created functions and/or create new functions).
+# Be sure to name the scripts aptly.
+
+import random
+def Mortgage_amounts():
+    return [random.randint(100, 1000) for iter in range(100)]
