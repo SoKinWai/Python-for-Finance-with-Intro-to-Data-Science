3bc6f1bacdfd06281be0aeb6a1b8fa8a85e932a5
SoKinWai <45207705+SoKinWai@users.noreply.github.com>
2022-04-10 14:51:33 -0500

    Add files via upload

---
+++ Level 1/Section 1.6_Packages/1.6.2/Sub_a/subsub_1/week_day_function/week_day_function.py
@@ -0,0 +1,9 @@
+#c. Create one or more Python scripts in each subfolder, at each level.
+# Each script should have a unique function (you may reuse previously-created functions and/or create new functions).
+# Be sure to name the scripts aptly.
+
+
+weekdays=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+def week_day_function(num):
+    return print((num,weekdays[num-1]))
