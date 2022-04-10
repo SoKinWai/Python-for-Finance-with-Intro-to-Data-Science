f32804a3164e43830fdda5eee3db38c8c1da7ff3
SoKinWai <45207705+SoKinWai@users.noreply.github.com>
2022-04-10 14:48:27 -0500

    Add files via upload

---
+++ Level 1/Section 1.6_Packages/1.6.1.py
@@ -0,0 +1,30 @@
+#Student name: Jianwei Su
+#Date: 04/06/2022
+#1.6.1
+
+import math
+
+def main():
+    # Code goes here
+    print('\n==========Exercise 1.6.1===========')
+
+    #Print dir(math) on screen.
+    print("To see what math module contains:",dir(math))
+
+    #Demonstrate usage of many of its built-in functions
+    print('cos function:',math.cos(3.14/3))
+
+    print('sin function:',math.sin(3.14/6))
+
+    print('tan function:',math.tan(1))
+
+    print('Exponential function:',math.exp(2))
+
+    print('Power function:',math.pow(3,2))
+
+    print('\n***Program Complete***')
+
+
+
+if __name__ == '__main__':
+    main()
