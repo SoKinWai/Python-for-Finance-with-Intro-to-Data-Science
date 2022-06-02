72361aee496d2f7e9cba3f19b0d8e760c0ce79f5
SoKinWai <45207705+SoKinWai@users.noreply.github.com>
2022-06-01 20:12:23 -0500

    Add files via upload

---
+++ Level 7/utils/memoize.py
@@ -0,0 +1,29 @@
+#Student Name: Jianwei Su
+#Level 7
+
+
+from functools import wraps
+
+
+
+
+
+#Create a decorator that memoize’s the result of a function.
+def memoize(f): #memoize function
+
+    #Note that memoizing should happen on a per-parameter basis
+    # This cache dict is to store every result
+    cache_dict = {}
+
+    #Nesting function
+    @wraps(f) # Decorator that decorates the wrapped function to retain the representation of the function
+    def wrapped(*args, **kwargs):  # Take all keyword and non-keyword parameters
+
+        #Set the cache key
+        cache_key=str(args)+str(kwargs)
+
+        if cache_key not in cache_dict:
+            cache_dict[cache_key] = f(*args, **kwargs)
+        return cache_dict[cache_key]
+
+    return wrapped
